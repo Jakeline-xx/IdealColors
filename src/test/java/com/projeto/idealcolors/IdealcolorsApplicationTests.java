bae373671278e67b0c62466a64c67e36fe2d588c
Jakeline-xx <jakeline.santana.rocha@gmail.com>
2023-05-20 01:09:46 -0300

    inicio

---
+++ src/test/java/com/projeto/idealcolors/IdealcolorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.idealcolors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdealcolorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
